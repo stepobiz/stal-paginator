b0b650dc053b509518a7f8ac7c008a39f98b94d3
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-12-21 11:05:54 +0100

    init

---
+++ src/lib/paginator.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PaginatorComponent } from './paginator.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaginatorComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    PaginatorComponent
+  ]
+})
+export class PaginatorModule { }
